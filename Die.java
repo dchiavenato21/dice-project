27e4364c10ce608cb28e490869f4a6d836d6c4af
Daniel Chiavenato <dchiavenato21@scarsdaleschools.org>
2019-11-01 08:47:25 -0400

    Added Die Class

---
+++ Die.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class Die here.
+ *
+ * @author (Daniel Chiavenato)
+ * @version (11/1/19)
+ */
+public class Die
+{
+    private int diceValue;
+    
+    /**
+     * Initializes the die with a random value
+     */
+    public Die()
+    {
+        roll();
+    }
+    
+    /**
+     * @return Current value of the die
+     */
+    public int getValue()
+    {
+        return diceValue;
+    }
+    
+    /**
+     * Randomizes the dice value
+     */
+    public void roll()
+    {
+        diceValue = (int)(Math.random() * 6 + 1);
+    }
+    
+    /**
+     * @return String format of current die value
+     */
+    @Override
+    public String toString()
+    {
+        return ("" + diceValue);
+    }
+}
